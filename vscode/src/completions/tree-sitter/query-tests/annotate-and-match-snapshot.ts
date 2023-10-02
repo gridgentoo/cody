e79726c2ccb4e97906e0709543a3cdd70baf2cd7
Valery Bugakov <skymk1@gmail.com>
2023-10-02 12:00:48 +0800

    Autocomplete: eslint 🙏

--- vscode/src/completions/tree-sitter/query-tests/annotate-and-match-snapshot.ts
+++ vscode/src/completions/tree-sitter/query-tests/annotate-and-match-snapshot.ts
@@ -112,7 +112,7 @@ function annotateSnippets(params: AnnotateSnippetsParams): string {
     const { code, language, captures, parser } = params
 
     const { delimiter, indent } = getCommentDelimiter(language)
-    const lines = code.split('\n').map(line => line.replace(/\t/g, ' '.repeat(4)))
+    const lines = code.split('\n').map(line => line.replaceAll(/\t/, ' '.repeat(4)))
     const caretPoint = getCaretPoint(lines, delimiter)
     if (!caretPoint) {
         throw new Error('No caret point found')
